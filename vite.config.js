529e46d9670d208356d2a4cad1eb6d0d093f0db9
Marsel <theo.marchell10@gmail.com>
2026-08-26 00:50:18 +0700

    Configure GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,8 @@ import react from '@vitejs/plugin-react'
 import { VitePWA } from 'vite-plugin-pwa'
 
 export default defineConfig({
+  base: '/Japan-OS/',
+
   plugins: [
     react(),
 
@@ -19,12 +21,12 @@ export default defineConfig({
 
         icons: [
           {
-            src: '/pwa-192x192.png',
+            src: '/Japan-OS/pwa-192x192.png',
             sizes: '192x192',
             type: 'image/png'
           },
           {
-            src: '/pwa-512x512.png',
+            src: '/Japan-OS/pwa-512x512.png',
             sizes: '512x512',
             type: 'image/png'
           }
@@ -32,14 +34,14 @@ export default defineConfig({
       },
 
       workbox: {
-  cleanupOutdatedCaches: true,
+        cleanupOutdatedCaches: true,
 
-  globPatterns: [
-    '**/*.{js,css,html,ico,png,svg,jpg,jpeg,webp}'
-  ],
+        globPatterns: [
+          '**/*.{js,css,html,ico,png,svg,jpg,jpeg,webp}'
+        ],
 
-  navigateFallback: 'index.html'
-}
+        navigateFallback: 'index.html'
+      }
     })
   ]
 })
